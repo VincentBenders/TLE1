93cdcd6f4dc9dbdf5a5716a62cc63daa8a530801
Sander <sanderlandm@gmail.com>
2024-09-18 13:39:56 +0200

    slightly more styling for the home page

--- includes/pages/templates/home.php
+++ includes/pages/templates/home.php
@@ -1,19 +1,21 @@
+<section id="debugHome">
 
+    <h1>Home</h1>
 
-<h1>Home</h1>
+    <p>This is the home page, you should be able to get everywhere else from here</p>
 
-<p>This is the home page, you should be able to get everywhere else from here</p>
+    <div id="debugLinks">
 
-<div id="debugLinks">
+        <a href="<?= BASE_PATH ?>create">create page</a>
+        <a href="<?= BASE_PATH ?>delete">delete page</a>
+        <a href="<?= BASE_PATH ?>details">details page</a>
+        <a href="<?= BASE_PATH ?>edit">edit page</a>
+        <a href="<?= BASE_PATH ?>login">login page</a>
+        <a href="<?= BASE_PATH ?>logout">logout page</a>
+        <a href="<?= BASE_PATH ?>register">register page</a>
+        <a href="<?= BASE_PATH ?>secure">secure page</a>
 
-<a href="<?= BASE_PATH ?>create">create page</a>
-<a href="<?= BASE_PATH ?>delete">delete page</a>
-<a href="<?= BASE_PATH ?>details">details page</a>
-<a href="<?= BASE_PATH ?>edit">edit page</a>
-<a href="<?= BASE_PATH ?>create">create page</a>
-<a href="<?= BASE_PATH ?>create">create page</a>
-<a href="<?= BASE_PATH ?>create">create page</a>
-<a href="<?= BASE_PATH ?>create">create page</a>
+    </div>
 
-</div>
+</section>
 
