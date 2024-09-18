ac86a6f4fea79f4cad350dbfbdba29a8f2bdcb3a
Sander <sanderlandm@gmail.com>
2024-09-18 11:53:29 +0200

    Added the settings file

---
+++ includes/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+//Copied code courtesy of Antwan
+//Paths
+const BASE_PATH = '/CLE2/'; //prg02-eindopdracht
+const INCLUDES_PATH = __DIR__ . '/';
+const DSN = 'mysql:host=localhost;dbname=tle_1_experimenteren';
+const USERNAME = 'root';
+const PASSWORD = '';
+
+//Custom error handler, so every error will throw a custom ErrorException
+set_error_handler(function ($severity, $message, $file, $line) {
+    throw new ErrorException($message, $severity, $severity, $file, $line);
+});
