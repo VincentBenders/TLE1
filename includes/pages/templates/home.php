0970fefe3dec28337e201501f94acee59fe0fdb4
Esse03 <essetijdelijk@gmail.com>
2024-09-18 14:32:10 +0200

    ...

--- includes/pages/templates/home.php
+++ includes/pages/templates/home.php
@@ -2,20 +2,24 @@
 
     <h1>Home</h1>
 
-    <p>This is the home page, you should be able to get everywhere else from here</p>
+    <div>
+        <p>This is the home page, you should be able to get everywhere else from here</p>
 
-    <div id="debugLinks">
+        <div id="debugLinks">
+            <block>
+                <a href="<?= BASE_PATH ?>create">create page</a>
+                <a href="<?= BASE_PATH ?>delete">delete page</a>
+                <a href="<?= BASE_PATH ?>details">details page</a>
+                <a href="<?= BASE_PATH ?>edit">edit page</a>
+            </block>
+            <block>
+                <a href="<?= BASE_PATH ?>login">login page</a>
+                <a href="<?= BASE_PATH ?>logout">logout page</a>
+                <a href="<?= BASE_PATH ?>register">register page</a>
+                <a href="<?= BASE_PATH ?>secure">secure page</a>
+            </block>
 
-        <a href="<?= BASE_PATH ?>create">create page</a>
-        <a href="<?= BASE_PATH ?>delete">delete page</a>
-        <a href="<?= BASE_PATH ?>details">details page</a>
-        <a href="<?= BASE_PATH ?>edit">edit page</a>
-        <a href="<?= BASE_PATH ?>login">login page</a>
-        <a href="<?= BASE_PATH ?>logout">logout page</a>
-        <a href="<?= BASE_PATH ?>register">register page</a>
-        <a href="<?= BASE_PATH ?>secure">secure page</a>
-        <a href="<?= BASE_PATH ?>tagCreate">create tag page</a>
-
+        </div>
     </div>
 
 </section>
