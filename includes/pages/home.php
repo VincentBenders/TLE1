626dceb554f375044722f087908516d73d7f5cfa
Sander <sanderlandm@gmail.com>
2024-09-25 12:20:55 +0200

    Piece of shit fucking code completion error fixed, go to hell

--- includes/pages/home.php
+++ includes/pages/home.php
@@ -6,7 +6,7 @@ $title = 'home';
 //Check if the user has logged in
 if (!isset($_SESSION['userId'])) {
     //If not, send them to the login page
-    header("Location: login.php");
+    header("Location: login");
     exit;
 }
 
