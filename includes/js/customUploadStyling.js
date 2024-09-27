a2d9dabd6c7f8f05239883ee6e50e1307c8e3529
Sander <sanderlandm@gmail.com>
2024-09-27 16:06:59 +0200

    Fully added styling to register pages

---
+++ includes/js/customUploadStyling.js
@@ -0,0 +1,43 @@
+//Wait for the page to load before doing anything
+window.addEventListener('load', init);
+
+//Global variables
+let fileNameContainer;
+let pencilIcon;
+
+//Initializing function
+function init() {
+
+    //Get all the DOM elements we need
+    fileNameContainer = document.getElementById('uploadedFileNameContainer');
+    pencilIcon = document.getElementById('pencilIcon');
+
+    //Add an event listener to the file upload input element
+    document.getElementById('image').addEventListener('change', showFileName);
+
+}
+
+
+//Functions
+
+//Displays the name of the file on the page and changes some styling
+function showFileName(e) {
+
+    if (e.target.files[0]) {
+        //If a file was uploaded
+        //Put the name in the container
+        fileNameContainer.innerText = e.target.files[0].name;
+
+        //Change the pencil icon's color to green
+        pencilIcon.classList.replace('grey', 'green');
+
+    } else {
+        //If no file was uploaded or the file was removed
+        //Empty out the name container
+        fileNameContainer.innerText = '';
+
+        //Change the pencil icon's color to grey
+        pencilIcon.classList.replace('green', 'grey');
+    }
+
+}
--- includes/pages/templates/register.php
+++ includes/pages/templates/register.php
@@ -1,49 +1,68 @@
+<script src="<?= BASE_PATH ?>/includes/js/customUploadStyling.js"></script>
+
 <main id="registerMain">
 
-    <h1>Maak een nieuw account aan</h1>
+    <h1>Create a new account</h1>
 
     <!-- Registratieformulier -->
-    <form action="" method="post" enctype="multipart/form-data">
+    <form action="" method="post" enctype="multipart/form-data" class="extrude">
         <!-- Naam -->
         <div>
-            <label for="name"></label>
-            <input type="text" placeholder="Naam" name="name" id="name"
+            <label for="name">Name</label>
+            <input type="text" placeholder="Name..." name="name" id="name"
                    value="<?= htmlentities($_POST['name'] ?? '') ?>">
         </div>
 
         <!-- E-mail -->
         <div>
-            <label for="email"></label>
-            <input type="text" placeholder="Email" name="email" id="email"
+            <label for="email">Email</label>
+            <input type="text" placeholder="Email..." name="email" id="email"
                    value="<?= htmlentities($_POST['email'] ?? '') ?>">
         </div>
 
         <!-- Wachtwoord -->
-        <div>
-            <label for="password">Wachtwoord</label>
-            <input type="password" placeholder="Wachtwoord" name="password" id="password"
-                   value="<?= htmlentities($_POST['password'] ?? '') ?>">
-        </div>
+        <div id="passwordRepeat">
 
-        <!-- Wachtwoord controle -->
-        <div>
-            <label for="passwordConfirm">Herhaal wachtwoord</label>
-            <input type="password" placeholder="Wachtwoord" name="passwordConfirm" id="passwordConfirm"
-                   value="<?= htmlentities($_POST['passwordConfirm'] ?? '') ?>">
+            <div>
+                <label for="password">Password</label>
+                <input type="password" placeholder="Password..." name="password" id="password"
+                       value="<?= htmlentities($_POST['password'] ?? '') ?>">
+            </div>
+
+            <!-- Wachtwoord controle -->
+            <div>
+                <label for="passwordConfirm">Repeat password</label>
+                <input type="password" placeholder="Password..." name="passwordConfirm" id="passwordConfirm"
+                       value="<?= htmlentities($_POST['passwordConfirm'] ?? '') ?>">
+            </div>
+
         </div>
 
         <!-- Profielfoto -->
 
         <div>
-            <label for="image">Profiel foto</label>
-            <input type="file" name="image" id="image">
+            <label for="image">Profiel picture</label>
+            <label id="customFileUpload">
+                Upload an image...
+                <input type="file" name="image" id="image">
+                <img src="<?= BASE_PATH ?>includes/images/pencil-icon.svg" alt="" class="grey" id="pencilIcon">
+                <span id="uploadedFileNameContainer"></span>
+            </label>
         </div>
 
 
         <!-- Submit-knop -->
-        <button type="submit" name="submit">Account aanmaken</button>
+        <button type="submit" name="submit" class="button">Create account
+            <img src="<?= BASE_PATH ?>includes/images/checkmark-icon.svg" alt="" class="grey">
+        </button>
+
     </form>
 
-    <h2>Heb je al een account?</h2>
-    <a href="<?= BASE_PATH ?>login">Log in</a>
+    <div>
+        <h2>Already have an account?</h2>
+        <a href="<?= BASE_PATH ?>login" class="button">Log in
+            <img src="<?= BASE_PATH ?>includes/images/arrow-right.svg" alt="" class="white">
+        </a>
+    </div>
+
 </main>
