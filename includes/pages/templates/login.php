9ded9efd6940573245e78920fe7a892ebe2f0743
Sander <sanderlandm@gmail.com>
2024-09-27 13:47:00 +0200

    Added full styling to login page

--- includes/pages/templates/login.php
+++ includes/pages/templates/login.php
@@ -2,7 +2,7 @@
 
     <h1>Log in</h1>
 
-    <form action="" method="post">
+    <form action="" method="post" class="extrude">
         <!-- Email -->
         <div>
             <label for="email">Email</label>
@@ -13,15 +13,17 @@
         <!-- Wachtwoord -->
         <div>
             <label for="password">Wachtwoord</label>
-
             <input type="password" name="password" id="password" required>
         </div>
 
         <!-- Submit Button -->
-        <input type="submit" name="submit" value="Log in">
+        <input type="submit" name="submit" value="Log in" class="button">
     </form>
 
-    <h2>Nog geen account?</h2>
-    <a href="<?= BASE_PATH ?>register">Maak een nieuw account aan</a>
+    <div>
+        <h2>Nog geen account?</h2>
+        <a href="<?= BASE_PATH ?>register" class="button">Maak een nieuw account aan <img
+                    src="<?= BASE_PATH ?>includes/images/arrow-right.svg" alt="" class="white"></a>
+    </div>
 
 </main>
