4bb0ea6587a9e84daeec9bba2350a3c91ebebf46
Sander <sanderlandm@gmail.com>
2024-10-01 14:02:19 +0200

    Login page now displays errors and success to user

--- includes/pages/templates/login.php
+++ includes/pages/templates/login.php
@@ -1,18 +1,40 @@
 <main id="loginMain">
 
-    <h1>Log in</h1>
+    <h1>Log in
+        <?php if (!empty($_SESSION['success'])) { ?>
+            <span id="success">
+            <img src="<?= BASE_PATH ?>includes/images/warning-icon.svg" alt="" class="green">
+            <?= $_SESSION['success'] ?>
+        </span>
+            <?php unset($_SESSION['success']); ?>
+        <?php } ?>
+    </h1>
 
     <form action="" method="post" class="extrude">
         <!-- Email -->
         <div>
-            <label for="email">Email</label>
+            <label for="email">Email
+                <?php if (!empty($validationErrors['email'])) { ?>
+                    <span class="error">
+                        <img src="<?= BASE_PATH ?>includes/images/warning-icon.svg" alt="" class="melon">
+                        <?= $validationErrors['email'] ?>
+                    </span>
+                <?php } ?>
+            </label>
             <input type="text" name="email" id="email"
                    value="<?= htmlentities($_POST['email'] ?? '') ?>" required>
         </div>
 
         <!-- Wachtwoord -->
         <div>
-            <label for="password">Password</label>
+            <label for="password">Password
+                <?php if (!empty($validationErrors['password'])) { ?>
+                    <span class="error">
+                        <img src="<?= BASE_PATH ?>includes/images/warning-icon.svg" alt="" class="melon">
+                        <?= $validationErrors['password'] ?>
+                    </span>
+                <?php } ?>
+            </label>
             <input type="password" name="password" id="password" required>
         </div>
 
@@ -20,13 +42,15 @@
         <button type="submit" name="submit" value="Log in" id="submit" class="button">
             Log in <img src="<?= BASE_PATH ?>includes/images/checkmark-icon.svg" alt="" class="grey">
         </button>
-        
+
     </form>
 
     <div>
         <h2>No account yet?</h2>
-        <a href="<?= BASE_PATH ?>register" class="button">Create a new account <img
-                    src="<?= BASE_PATH ?>includes/images/arrow-right.svg" alt="" class="white"></a>
+        <a href="<?= BASE_PATH ?>register" class="button">
+            Create a new account
+            <img src="<?= BASE_PATH ?>includes/images/arrow-right.svg" alt="" class="white">
+        </a>
     </div>
 
 </main>
